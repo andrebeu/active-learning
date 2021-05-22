80d6c3640b70e320d0f0a643e6c92f14f9600c0f
andrebeu <aobeukers@gmail.com>
2021-05-22 11:35:55 -0400

    cleanup; redid task objects; init env object; working on agent obj

--- pwm-toy0521/utils.py
+++ pwm-toy0521/utils.py
@@ -5,87 +5,107 @@ from torch.distributions.categorical import Categorical
 
 
 Exp = namedtuple('Experience',[
-    'tstep','state','action','reward','state_tp1','rnn_state'
-    ],defaults=[None]*6)
+    'state','obs','action','reward',
+    'obs_tp1','rnn_state'
+    ], defaults=[None]*2)
+
+
+
+class Env():
+    def __init__(self):
+        None
+    
+    def play_trial(self,agentpi,task):
+        """ 
+        action <- agentpi(state)
+        task methods: sample_trial, reward_fn 
+        """
+        trial_st,trial_obs = task.sample_trial()
+        expL = []
+        final_state = False
+        for tstep in range(len(trial_st)):
+            if tstep == len(trial_st)-1:
+                final_state = True
+            ## play
+            st = trial_st[tstep]
+            ot = trial_obs[tstep]
+            at = agentpi(ot)
+            rt = task.reward_fn(st,at)
+            # final state
+            if final_state:
+                otp = -1
+            else:
+                otp = trial_st[tstep+1]
+            # record
+            expt = Exp(st,ot,at,rt,otp)
+            expL.append(expt)
+        return expL
+
 
 """ 
 WM tasks are POMDP environments in which state(t+1) do not depend on action
 implication: unlike traditional RL, environment and agent can be disentangled 
+
+distinguishing state from observation becasue POMDP
+    R/L response depends on comparison between 
+    first and last obs
 """
 
 class PWMTask():
     
     
-    def __init__(self,stim_set):
+    def __init__(self,stim_set,delay):
+        """ action space {0:hold,1:left,2:right}
+        """
         self.stim_set = stim_set
+        self.delay = delay
         self.action_set = [0,1,2]
-        self.randpi_fn = lambda x: np.random.choice(len(self.action_set))
         return None
-
-    def reset_trial(self):
-        """ initialize vars between trials"""
-        self.tstep = 0
-        self.state = 0
-        return None
-
-    def reward_fn(self,st,at):
-        rt = 0
-        return rt
-
-    def step(self):
-        None
 
     def sample_trial(self):
         """ 
-        distinguishing state from observation becasue POMDP
-            R/L response depends on comparison between 
-            first and last obs
-        trial_st
-        instructs rewarded action 
-        0 is hold, 1 is 1, 2 is R
-        reward hold everywhere except 
-        after second stimulus
         """
-        self.reset_trial()
         SAi,SBi = self.stim_set[np.random.choice(len(self.stim_set))]
-        SA,SB = SAi,SBi # int coding
-        delay = 2
-        trial_obs = np.zeros(delay+3) # two stim + final delay
+        # embed stim
+        SA,SB = SAi,SBi 
+        trial_obs = np.zeros(self.delay+2) # two stim
         trial_obs[0] = SA
-        trial_obs[-2] = SB
-        trial_st = np.zeros(delay+3)
+        trial_obs[-1] = SB
+        # rewarded action 
+        trial_st = np.zeros(self.delay+2)
         trial_st[-1] = 1+int(SA>SB) 
         return trial_st,trial_obs
 
-    def play_trial(self,pifn=None):
-        """ deprecated. leaving for now
+    def reward_fn(self,st,at):
+        """ 
+        state number indicates rewarded action
+        reward hold everywhere except 
+         after second stimulus
         """
-        if type(pifn)==type(None):
-            pifn = self.randpi_fn
-        trial_st,trial_obs = self.sample_trial()
-        expL = []
-        for tstep,(st,ot) in enumerate(zip(trial_st,trial_obs)):
-            at = pifn(ot)
-            rt = self.reward_fn(st,at)
-            if tstep == len(trial_st)-1:
-                stp = -1
-            else:
-                stp = trial_st[tstep+1]
-            expt = Exp(tstep,st,at,rt,stp)
-            expL.append(expt)
-            print(expt)
-        return expL
-    
-    
+        if st == at:
+            rt = 1
+        else:
+            rt = 0
+        return rt
 
+    
 """ 
+implementation NOTEs: 
+
+two modes:
+ rnn_step can be used online
+  RNN state held as instance variable
+ while rnn_unroll is more efficient
+  returns states for every obs in sequence
+
+
 TODO: restructure learning target setup to allow 
 TD lambda continuum between onestep TD and MC 
 """
 
 class ActorCritic(tr.nn.Module):
   
-    def __init__(self,indim=4,nactions=2,stsize=18,gamma=0.80,learnrate=0.005,TDupdate=False):
+    def __init__(self,indim=1,nactions=3,stsize=15,gamma=0.80,learnrate=0.005,TDupdate=False):
         super().__init__()
         self.indim = indim
         self.stsize = stsize
@@ -110,60 +130,42 @@ class ActorCritic(tr.nn.Module):
         )
         return None
 
-    def unroll_trial(self,trial):
-        """ 
-        trial is tuple (states,observations)
-        """
-        trial_st,trial_obs = trial
-        for tstep in range(len(trial_st)):
-            st,ot = trial_st[tstep],trial_obs[tstep]
-            at = self.act(ot)
+    def reset_state(self):
+        self.h_t,self.c_t = self.rnn_st0
         return None
 
-
-    def unroll_ep(self,task):
-        """ actor logic 
+    def act(self,obs):
+        """ 
+        pi_act [batch,nactions] is output of policy head
+        which gets passed through a softmax 
+        and from which an action is sampled
         """
-        finalst = False
-        task.reset()
-        self.h_t,self.c_t = self.rnn_st0
-        EpBuff = []
-        action = np.random.binomial(1,0.5)
-        while not finalst:
-            obs,r_t,finalst = task.step(action)
-            h_t = self.rnn_step(obs)
-            vh_t = self.rnn2val(h_t)
-            pih_t = self.rnn2pi(h_t) 
-            action = self.act(pih_t)
-            exp = Exp('tstep',obs[0],action,r_t,obs[0]+1,h_t)
-            EpBuff.append(exp)
-        return EpBuff
+        hstate = self.rnn_step(obs)
+        pi_act = self.rnn2pi(hstate_t)
+        pism = pi_act.softmax(-1)
+        pidistr = Categorical(pism)
+        action = pidistr.sample()
+        return action
 
     def rnn_step(self,obs):
+        """ 
+        updates rnn state instance variable
+         flexible: used for online 
+        """
         obs = tr.Tensor(obs).unsqueeze(0)
         self.h_t,self.c_t = self.rnn(obs,(self.h_t,self.c_t))
         return self.h_t
 
-    def act(self,pi_out):
-        """ pi_out [batch,nactions] is output of policy head
+    def rnn_unroll(self,obsL):
+        """ 
+        given sequence of stimuli, unrolls rnn
+         returns hidden state
+        efficient: useful when applying output layer
+         in paralelle
         """
-        pism = pi_out.softmax(-1)
-        pidistr = Categorical(pism)
-        actions = pidistr.sample()
-        return actions
 
-    def eval(self,expD):
-        """ """
-        data = {}
-        ## entropy
-        vhat,pact = self.forward(expD['state'])
-        pra = pact.softmax(-1)
-        entropy = -1 * tr.sum(pra*pra.log2(),-1).mean()
-        data['entropy'] = entropy.detach().numpy()
-        ## value
-        returns = compute_returns(expD['reward']) 
-        data['delta'] = np.mean(returns - vhat.detach().numpy())
-        return data
+        return None
+
 
     def update(self,expD):
         """ REINFORCE and A2C updates
@@ -195,6 +197,50 @@ class ActorCritic(tr.nn.Module):
         los.backward()
         self.optiop.step()
         return None 
+
+    ### 
+
+    def unroll_trial(self,trial):
+        """ 
+        trial is tuple (states,observations)
+        """
+        trial_st,trial_obs = trial
+        for tstep in range(len(trial_st)):
+            st,ot = trial_st[tstep],trial_obs[tstep]
+            at = self.act(ot)
+        return None
+
+    def unroll_ep(self,task):
+        """ actor logic 
+        """
+        finalst = False
+        task.reset()
+        self.h_t,self.c_t = self.rnn_st0
+        EpBuff = []
+        action = np.random.binomial(1,0.5)
+        while not finalst:
+            obs,r_t,finalst = task.step(action)
+            h_t = self.rnn_step(obs)
+            vh_t = self.rnn2val(h_t)
+            pih_t = self.rnn2pi(h_t) 
+            action = self.act(pih_t)
+            exp = Exp('tstep',obs[0],action,r_t,obs[0]+1,h_t)
+            EpBuff.append(exp)
+        return EpBuff
+
+    def eval(self,expD):
+        """ """
+        data = {}
+        ## entropy
+        vhat,pact = self.forward(expD['state'])
+        pra = pact.softmax(-1)
+        entropy = -1 * tr.sum(pra*pra.log2(),-1).mean()
+        data['entropy'] = entropy.detach().numpy()
+        ## value
+        returns = compute_returns(expD['reward']) 
+        data['delta'] = np.mean(returns - vhat.detach().numpy())
+        return data
+
 
 
 def compute_returns(rewards,gamma=1.0):
